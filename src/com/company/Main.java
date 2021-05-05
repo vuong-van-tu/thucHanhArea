05364e1103fb949fc62eca0b902c38d8f89b5d90
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-05 14:03:28 +0700

    'thucHanhArea'

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width : ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = height * width ;
+        System.out.println("Area is : "+area);
+    }
+}
